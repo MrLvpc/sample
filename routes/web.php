8788308babb49ae9921a9446fa59d5a809367637
MrLvpc <392754345@qq.com>
2018-03-06 03:28:58 +0000

    Finish user edit, update, index, and destroy actions

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,13 @@ Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
+Route::get('/users', 'UsersController@index')->name('users.index');
 Route::get('signup', 'UsersController@create')->name('signup');
 Route::get('/users/{user}', 'UsersController@show')->name('users.show');
 Route::post('/users', 'UsersController@store')->name('users.store');
+Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+Route::post('/users/{user}/update', 'UsersController@update')->name('users.update');
+Route::post('/users/{user}/destroy', 'UsersController@destroy')->name('users.destroy');
 
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
